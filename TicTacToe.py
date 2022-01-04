695d45b774a991917fe50a6e2953071401918ecb
Vamsi Garghi <Garghivamsi@gmail.com>
2022-01-03 20:13:35 -0600

    implemented the mixmax algorithm and the program runs smoothly

--- TicTacToe.py
+++ TicTacToe.py
@@ -45,6 +45,26 @@ def checkWin ():
     else:
         return False
 
+def checkWhoWon (value):
+    if (board[1] == board[2] and board[1] == board[3] and board[1] != 'value'):
+        return True
+    elif (board[4] == board[5] and board[4] == board[6] and board[4] != 'value'):
+        return True
+    elif (board[7] == board[8] and board[7] == board[9] and board[7] != 'value'):
+        return True
+    elif (board[1] == board[4] and board[1] == board[7] and board[1] != 'value'):
+        return True
+    elif (board[2] == board[5] and board[2] == board[8] and board[2] != 'value'):
+        return True
+    elif (board[3] == board[6] and board[3] == board[9] and board[3] != 'value'):
+        return True
+    elif (board[1] == board[5] and board[1] == board[9] and board[1] != 'value'):
+        return True
+    elif (board[7] == board[5] and board[7] == board[3] and board[7] != 'value'):
+        return True
+    else:
+        return False
+
 def main(letter,position):
     if freeSpace(position):
         board[position] = letter
@@ -76,9 +96,51 @@ def forPlayerMove():
     return
 
 def forComputerMove ():
-    position = int(input("enter position for 'X'"))
-    main(comp,position)
+    bestScore = -1000
+    bestMove = 0
+
+    for key in board.keys():
+        if(board[key]== ' '):
+            board[key] = comp
+            score = minimax(board,0,False)
+            board[key] = ' '
+            if(score > bestScore):
+                bestScore = score
+                bestMove = key
+    main(comp,bestMove)
     return
+
+def minimax (board, depth, isMax):
+    if checkWhoWon (comp):
+        return 100
+    elif checkWhoWon(player):
+        return -100
+    elif checkDraw():
+        return 0
+    if isMax:
+        bestScore = -1000
+
+        for key in board.keys():
+            if(board[key]== ' '):
+                board[key] = comp
+                score = minimax(board,0,False)
+                board[key] = ' '
+                if(score > bestScore):
+                    bestScore = score2
+                    
+
+        return bestScore
+
+    else:
+        bestScore = 800
+        for key in board.keys():
+            if(board[key]== ' '):
+                board[key] = comp
+                score = minimax(board,depth + 1,True)
+                board[key] = ' '
+                if(score < bestScore):
+                    bestScore = score
+        return bestScore
 
 while not checkWin():
     forComputerMove()
